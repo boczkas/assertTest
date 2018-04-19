f58515f217481e8e516033df993a8a799f85d607
boczkas <przemyslawjakubowski@gmail.com>
2018-04-19 11:25:18 +0200

    ANSI_RESET removed

--- src/test/java/Color.java
+++ src/test/java/Color.java
@@ -1,5 +1,4 @@
 public class Color {
-        public static final String ANSI_RESET = "\u001B[0m";
         public static final String ANSI_BLACK = "\u001B[30m";
         public static final String ANSI_RED = "\u001B[31m";
         public static final String ANSI_GREEN = "\u001B[32m";
