301c182092902f86936d182929e8fe5a3555151b
boczkas <przemyslawjakubowski@gmail.com>
2018-04-19 11:48:17 +0200

    Colors changed to Enum

--- src/test/java/Color.java
+++ src/test/java/Color.java
@@ -1,11 +1,23 @@
-public class Color {
-        public static final String ANSI_BLACK = "\u001B[30m";
-        public static final String ANSI_RED = "\u001B[31m";
-        public static final String ANSI_GREEN = "\u001B[32m";
-        public static final String ANSI_YELLOW = "\u001b[33m";
-        public static final String ANSI_BLUE = "\u001B[34m";
-        public static final String ANSI_PURPLE = "\u001B[35m";
-        public static final String ANSI_CYAN = "\u001B[36m";
-        public static final String ANSI_WHITE = "\u001b[37m";
+public enum Color {
+    // codes for specyfic colors
+    
+        ANSI_RED("\u001B[31m"),
+        ANSI_GREEN("\u001B[32m"),
+        ANSI_YELLOW("\u001b[33m"),
+        ANSI_BLUE("\u001B[34m"),
+        ANSI_PURPLE("\u001B[35m"),
+        ANSI_CYAN("\u001B[36m"),
+        ANSI_WHITE("\u001b[37m");
+
+    String code;
+
+    Color(String colorCode) {
+        code = colorCode;
+    }
+
+    @Override
+    public String toString() {
+        return this.code;
+    }
 }
 
